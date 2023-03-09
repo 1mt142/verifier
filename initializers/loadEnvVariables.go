5ebf78f44331110dad3a3a95360473a22561c062
Amjad Hossain <amjad.hossain@upaybd.com>
2023-03-09 11:15:27 +0600

    compose update

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -3,9 +3,14 @@ package initializers
 import (
 	"github.com/joho/godotenv"
 	"log"
+	"os"
 )
 
 func LoadEnvVariables() {
+	if os.Getenv("ENVIRONMENT") == "docker" {
+		return
+	}
+
 	err := godotenv.Load()
 
 	if err != nil {
